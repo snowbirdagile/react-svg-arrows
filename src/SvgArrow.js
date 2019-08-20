ae222164cd05d6a6825dead8306d8cffa18ffeae
anuradha <saggunasekara@gmail.com>
2019-08-20 16:52:56 +0530

    Fix type errors

--- src/SvgArrow.js
+++ src/SvgArrow.js
@@ -117,9 +117,7 @@ export function computeLabelDimensions(
 }
 
 export function handleClick(sourceId: string, targetId: string, onArrowClick: Function) {
-  if((onArrowClick !== undefined && sourceId) && targetId) {
-    onArrowClick(sourceId, targetId);
-  }
+  onArrowClick(sourceId, targetId);
 }
 
 
